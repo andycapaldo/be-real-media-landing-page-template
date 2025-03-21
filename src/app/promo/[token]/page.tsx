a835a43cdd60c9e98d4bb04ae8bc3f3096a5efc8
Andrew Capaldo <andycapaldo2@gmail.com>
2025-03-21 14:56:55 -0600

    Fixed up landing page, changed styling and added more static details

--- src/app/promo/[token]/page.tsx
+++ src/app/promo/[token]/page.tsx
@@ -71,13 +71,21 @@ export default async function PromoPage(props: any) {
 
     return (
         <div className="min-h-screen flex flex-col items-center justify-center p-8 font-sans bg-cyan-50">
-            <title>{companyData.companyName}</title>
+            <h1 className='text-3xl font-bold text-center mb-15'>THANK YOU FOR VISITING THE {companyData.companyName.toUpperCase()} PREMIUM VIDEO PAGE</h1>
             <img
             src={companyData.logoUrl}
             alt={`${companyData.companyName} logo`}
-            className="max-w-xs mb-8"
+            className="max-w-xs mb-13"
             />
-            <h1 className="text-3xl font-bold mb-4 text-center">Welcome, {companyData.companyName}</h1>
+            <h1 className='text-xl font-bold text-center mb-5'>Below You Will Find (3 Mins):</h1>
+            <ul className='list-decimal list-inside text-center mb-8'>
+                <li className='text-lg font-semibold mb-5'>Research Based on your target audience and service areas</li>
+                <li className='text-lg font-semibold mb-5'>The Google Problem. Are you overpaying on Google?</li>
+                <li className='text-lg font-semibold mb-5'>Why StreamingTV is dominating?</li>
+                <li className='text-lg font-semibold mb-5'>What makes BeReal different?</li>
+                <li className='text-lg font-semibold mb-5'>Trivia</li>
+            </ul>
+            <h1 className='text-xl font-bold text-center mb-5'>But first, a quick hello from our Co-Founder to the {companyData.companyName} Team</h1>
             <div className="w-full max-w-md my-8">
                 <YouTubeEmbed url={companyData.videoUrl} />
             </div>
