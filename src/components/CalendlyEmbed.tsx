c028f934e2a235aee2be4eb635ed1456b2863bf1
Andrew Capaldo <andycapaldo2@gmail.com>
2025-04-01 15:44:41 -0600

    Fixed Calendly component to hide GPDR banner

--- src/components/CalendlyEmbed.tsx
+++ src/components/CalendlyEmbed.tsx
@@ -6,7 +6,7 @@ export default function CalendlyEmbed() {
     return (
         <div className="w-full h-screen">
             <InlineWidget
-                url="https://calendly.com/ryan-berealmediagroup" // Replace with your Calendly link
+                url="https://calendly.com/ryan-berealmediagroup?hide_gdpr_banner=1" // Replace with your Calendly link
                 styles={{ height: '100%', width: '100%' }}
             />
         </div>
