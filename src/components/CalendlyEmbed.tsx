91a5063055782faaa18c6813b627b4ede233e3c0
Andrew Capaldo <andycapaldo2@gmail.com>
2025-04-01 14:38:56 -0600

    Updated calendly link

--- src/components/CalendlyEmbed.tsx
+++ src/components/CalendlyEmbed.tsx
@@ -6,7 +6,7 @@ export default function CalendlyEmbed() {
     return (
         <div className="w-full h-screen">
             <InlineWidget
-                url="https://calendly.com/andycapaldo2" // Replace with your Calendly link
+                url="https://calendly.com/ryan-berealmediagroup" // Replace with your Calendly link
                 styles={{ height: '100%', width: '100%' }}
             />
         </div>
